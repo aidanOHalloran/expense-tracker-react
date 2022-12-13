e7c0e93dbd4c98d9b81115b21ecc5fdbff781bca
aidanOHalloran <amohalloran@crimson.ua.edu>
2022-12-13 12:09:54 -0600

    k

---
+++ client/src/components/Balance.js
@@ -0,0 +1,18 @@
+import React, { UseContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { useContext } from 'react';
+
+
+export const Balance = () => {
+  const { transactions } = useContext(GlobalContext); // bring transactions list
+
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  return (
+    <>
+    <h4>Your Balance</h4>
+    <h1>${total}</h1>
+    </>
+  )
+}
